34f4e9c43530e08e965e319b50db9bb2734466bc
Vinicius Gonçalves <69488844+vwnxvinicius@users.noreply.github.com>
2024-03-25 10:25:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+const scoreHolder = document.querySelector("#score_holder");
+const keys = {
+  w: { pressed: false },
+  s: { pressed: false },
+  a: { pressed: false },
+  d: { pressed: false },
+};
+let score = 0;
+
+canvas.width = 500;
+canvas.height = innerHeight - 20;
+
+const roadBackground = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/road_background.png",
+});
+const player = new Player({
+  imageSrc: "./img/car.png",
+});
+
+const enemies = [];
+
+gameLoop();
